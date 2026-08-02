e1cbd68a984d2b31323a528df9bc83d12685f14a
Hizbullah <hizbullah@gmail.com>
2026-08-02 14:17:32 +0500

    fix: restore app_colors.dart lost during rebase

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1565C0);
+  static const Color background = Color(0xFFF6F6F6);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFF1A1A1A);
+  static const Color textSecondary = Color(0xFF6B6B6B);
+  static const Color border = Color(0xFFE0E0E0);
+}
